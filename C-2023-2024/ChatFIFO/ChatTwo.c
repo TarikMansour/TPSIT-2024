95720e8dd97a1293129eb33fbce850298869ef96
TarikMansour <121286624+TarikMansour@users.noreply.github.com>
2024-04-04 11:10:14 +0200

    Update ChatTwo.c

--- C-2023-2024/ChatFIFO/ChatTwo.c
+++ C-2023-2024/ChatFIFO/ChatTwo.c
@@ -4,38 +4,31 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#define DIM 1024
 int main() {
-    char message[100];
-    char messageread[100];
-    int fd0;
-
-
-    //FIFO BIDIREZIONALE
-    fd0 = open("shell1_fifo", O_RDWR);
-    while (1) {
-        //USO SLEEP PER DARE IL TEMPO ALLA SHELL 1 DI SCRIVERE
-        sleep(1);
-        read(fd0, messageread, sizeof(messageread));
-        //SCRIVE MESSAGGI LETTI
-        printf("Shell 2 (Receiver): %s", messageread);
-        //SCRITTURA SUL MESSAGE 
-        printf("Shell 2 (Sender): ");
-        fgets(message, sizeof(message), stdin);
-        
-        //SCRITTURA E LETTURA DEI MESSAGGI SULLA FIFO
-        write(fd0, message, sizeof(message));
-        
-         //VERIFICA SE SHELL 2 HA SCRITTO HALT -> FERMA
-        if (strcmp(message, "HALT\n") == 0) {
-            printf("Shell 2 (Sender) stopping...\n");
-            break;
-        }
-      
-        
-       
-     
+    char arr1[DIM];
+    char arr2[DIM];
+    int fd;
+    int p;
+    p = fork();
+    if (p == -1){
+        return -1;
     }
-    //CHIUSURA FIFO
-    close(fd0);
+    if (p >0){
+        fd = open("fifone", O_WRONLY);
+        while(1){
+            fgets(arr2,DIM,stdin);
+            write(fd, arr2, strlen(arr2)+1);
+        }
+        close(fd);
+    }
+    else{
+        fd = open("fifone2", O_RDONLY);
+        while(strcmp(arr1,"HALT\n")){
+           read(fd, arr1, sizeof(arr1));
+           printf("chat2 : %s", arr1);
+        }
+        close(fd);
+    }
     return 0;
 }
