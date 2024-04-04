e6734572c52842ed9c7e0253aa5fd73a4d571278
TarikMansour <121286624+TarikMansour@users.noreply.github.com>
2024-04-04 10:52:03 +0200

    Update produttore.c

--- file-to-file_using-fifo/produttore.c
+++ file-to-file_using-fifo/produttore.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include <fcntl.h>
@@ -6,7 +7,8 @@
 #include<string.h>
 #include<sys/wait.h>
 #define BUFFER_DIM 1024
-#define PROGRAM "./esecuzione.exe"
+#define PROGRAM "./esecuzione"
+
 int spawn(char program[], char*argument[]){
     int p;
     p = fork();
@@ -16,7 +18,14 @@ int spawn(char program[], char*argument[]){
     if (p >0){
         return 0;
     }
-    execv(program, argument); //??
+    execv(program, argument); //equivalente a ("./esecuzione,"testo2.txt", NULL)
+                                /*execl("percorso", "nome del file", "stringa di ingresso", "numero di ingresso");
+                                char *mioarray[];
+                                mioarray[0] = "finestra";
+                                mioarray[1] = "ciaociao";
+                                mioarray[2] = "4";
+                                mioarray[3] = NULL;
+                                execv("percorso", mioarray); */
     abort();
 }
 int main(int argc, char *argv[]){
@@ -24,16 +33,17 @@ int main(int argc, char *argv[]){
     int fd;
     unsigned char buffer[BUFFER_DIM];
     int n;
-    char *arg[3]; //??
+    char *arg[3]; 
     if(argc != 3){
         printf("uso %s", argv[0]);
         return 0;
     }
-    arg[0] = (char*)malloc(strlen(PROGRAM)+1);
-    strcpy(arg[0], PROGRAM);
-    arg[1] = (char*)malloc(strlen(argv[2])+1);
-    strcpy(arg[1], argv[2]);
-    arg[2] = NULL;
+
+    arg[0] = (char*)malloc(strlen(PROGRAM)+1); 
+    strcpy(arg[0], PROGRAM); //presente il nome del programma
+    arg[1] = (char*)malloc(strlen(argv[2])+1); 
+    strcpy(arg[1], argv[2]); //terzo parametro di argv: file di destinazione
+    arg[2] = NULL; 
     if(spawn(PROGRAM,arg) <0){
         printf("errore creazione processo \n");
         free(arg[0]);
@@ -47,6 +57,7 @@ int main(int argc, char *argv[]){
         free(arg[1]);
         return 0;
     }
+
     file = fopen(argv[1], "rb");
     if(file == NULL){
         printf("errore apertura file %s \n", argv[1]);
@@ -54,6 +65,7 @@ int main(int argc, char *argv[]){
         free(arg[1]);
         return 0;
     }
+
     while(n=fread(buffer,1,sizeof(buffer),file)>0){
         write(fd, buffer, n);
     }
@@ -63,3 +75,4 @@ int main(int argc, char *argv[]){
     free(arg[1]);
     return 1;
 }
+
