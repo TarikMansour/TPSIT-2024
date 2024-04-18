fc8065fea88afcf7432f959d825fe7c7f726d5c6
TarikMansour <121286624+TarikMansour@users.noreply.github.com>
2024-04-18 11:21:11 +0200

    Update programmaricerca.c

--- programmaricerca.c
+++ programmaricerca.c
@@ -2,31 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+struct Parametri{
+    int indice;
+    int ultimo;
+    int numero;
+};
 int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-void* ricerca1(void* arg){
-    int* array = (int*)arg;
-    for (int i =0; i<5;i++){
-        if (array[i] == 8){
+void* ricerca(void* arg){
+    struct Parametri *par = (struct Parametri*)arg;
+    for (int i =par->indice; i<=par->ultimo;i++){
+        if (arr[i] == par->numero){
             printf("prima meta dell'array: \n");
             printf("numero trovato nella posizione %d dell'array \n", i+1);
         }
     }
     pthread_exit(NULL);
 }
-void* ricerca2(void* arg){
-    int* array = (int*)arg;
-    for (int i =5; i<10;i++){
-        if (array[i] == 8){
-            printf("seconda meta dell'array: \n");
-            printf("numero trovato nella  posizione %d dell'array \n", i+1);
-        }
-    }
-    pthread_exit(NULL);
-}
+
 int main(){
+    struct Parametri par_1, par_2;
+    par_1.indice = 0;
+    par_1.ultimo = 5;
+    par_1.numero = 8;
+    par_2.indice = 5;
+    par_2.ultimo = 10;
+    par_2.numero = 8;
     pthread_t threadRicerca1, threadRicerca2;
-    pthread_create(&threadRicerca1, NULL, &ricerca1,(void*)arr);
-    pthread_create(&threadRicerca2, NULL, &ricerca2, (void*)arr);
+    pthread_create(&threadRicerca1, NULL, &ricerca,&par_1);
+    pthread_create(&threadRicerca2, NULL, &ricerca,&par_2);
     pthread_join(threadRicerca1, NULL);
     pthread_join(threadRicerca2, NULL);
     return 0;
